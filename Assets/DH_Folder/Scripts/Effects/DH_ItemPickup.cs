a037907f843109fc718220f2b375bd88ff0eeb27
leeworm <kalrocse@gmail.com>
2025-05-12 16:07:17 +0900

    아이템픽업

--- Assets/DH_Folder/Scripts/Effects/DH_ItemPickup.cs
+++ Assets/DH_Folder/Scripts/Effects/DH_ItemPickup.cs
@@ -8,7 +8,7 @@ public class DH_ItemPickup : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            Vector3 spawn = new Vector3(collision.transform.position.x, 0, 0);
+            Vector3 spawn = new Vector3(collision.transform.position.x, collision.transform.position.y, 0);
             Instantiate(portalPrefab, spawn, Quaternion.identity);
             gameObject.SetActive(false); // 또는 Destroy(gameObject);
         }
