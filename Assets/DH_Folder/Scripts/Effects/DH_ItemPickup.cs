9771f6a9c88c3d2f8cb3beffdf1b285c590711fa
leeworm <kalrocse@gmail.com>
2025-05-12 15:56:18 +0900

    아이템 픽업 스크립트 수정

--- Assets/DH_Folder/Scripts/Effects/DH_ItemPickup.cs
+++ Assets/DH_Folder/Scripts/Effects/DH_ItemPickup.cs
@@ -2,16 +2,15 @@
 
 public class DH_ItemPickup : MonoBehaviour
 {
-    public DH_Player player;
     public GameObject portalPrefab;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Vector3 spawn = new Vector3(player.transform.position.x - 8f, 0, 0);
+            Vector3 spawn = new Vector3(collision.transform.position.x, 0, 0);
             Instantiate(portalPrefab, spawn, Quaternion.identity);
-            gameObject.SetActive(false); // or Destroy(gameObject);
+            gameObject.SetActive(false); // 또는 Destroy(gameObject);
         }
     }
 }
